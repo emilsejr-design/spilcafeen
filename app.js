d5c5568f7116e21b2977ec64d78a24ce3c2b844b
emilsejr-design <emilerop@gmail.com>
2026-04-23 10:41:23 +0200

    first JS commit

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 "use strict"
+const GAMES_URL = "https://raw.githubusercontent.com/cederdorff/race/refs/heads/master/data/games.json"
+let allGames = [];
+
+const gameList = document.querySelector("#game-list");
+const genreSelect = document.querySelector("#genre-select");
+const searchInput = document.querySelector("#search-input");
+const sortSelect = document.querySelector("#sort-select");
+const gameCount = document.querySelector("#game-count");
+
+fetchGames();
+
